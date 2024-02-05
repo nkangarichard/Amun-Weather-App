5735e8934185c48bb1d218daade9e097887f5dde
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2024-02-05 13:52:02 -0500

    Initial Commit

---
+++ Amun/AmunApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmunApp.swift
+//  Amun
+//
+//  Created by Richard Nkanga on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AmunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Amun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Amun
+//
+//  Created by Richard Nkanga on 05/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
