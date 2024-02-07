8d5a79926cdaf2d08520822e2ae3180b29f9f91d
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2024-02-06 21:30:18 -0500

    made modification to sample onboarding screen

--- Amun/ContentView.swift
+++ Amun/ContentView.swift
@@ -9,14 +9,96 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Color(.background)
+
+                VStack {
+                    ZStack {
+                        Image("sun")
+                    }
+
+                    VStack {
+                        Text("Know Everything about the weather").font(.custom("Avenir-Black", size: 37))
+
+                        VStack {
+                            Text("Start now and learn more about the")
+                                .font(.custom("Avenir-Medium", size: 17))
+
+                            HStack(spacing: 5) {
+                                Text("local weather")
+                                    .font(.custom("Avenir-Black", size: 17))
+
+                                Text("instantly")
+                                    .font(.custom("Avenir-Medium", size: 17))
+                            }
+                        }
+                        .opacity(0.5)
+                    }
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.text)
+
+                    Button(action: {
+                        // code
+
+                    }, label: {
+                        Text("Get Started")
+                            .font(.custom("Avenir-Black", size: 17))
+                            .frame(width: 260, height: 60, alignment: .center)
+
+                            .background {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .frame(width: 260, height: 60)
+                                        .foregroundStyle(.greyShadow)
+                                        .offset(x: -6, y: 6)
+
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .frame(width: 260, height: 60)
+                                        .foregroundStyle(.text)
+                                }
+                            }
+                            .foregroundColor(.textColorInversed)
+
+                    })
+                }
+                .padding()
+
+            }
+        .ignoresSafeArea(.all)
+        .toolbar{
+            ToolbarItem{
+
+                Button(action: {
+                    // code
+
+                }, label: {
+                    Text("Skip")
+                        .font(.custom("Avenir-Medum", size: 10))
+                        .frame(width: 58, height: 27, alignment: .center)
+
+                        .background {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 58, height: 27)
+                                    .foregroundStyle(.greyShadow)
+                                    .offset(x: -6, y: 6)
+
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 58, height: 27)
+                                    .foregroundStyle(.text)
+                            }
+                        }
+                        .foregroundColor(.textColorInversed)
+
+                })
+
+            }
         }
-        .padding()
+        }
+
     }
+
 }
 
 #Preview {
