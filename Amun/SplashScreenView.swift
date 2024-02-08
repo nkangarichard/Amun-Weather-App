5b5dc259c9fc51a3f3afe923386131d9669123b9
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2024-02-07 21:49:34 -0500

    added view for the splashscreen

---
+++ Amun/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  Amun
+//
+//  Created by Richard Nkanga on 07/02/2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
