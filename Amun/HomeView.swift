39741c59b34b385eaf15a4bdd1270d61088215e7
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2024-02-09 19:55:32 -0500

    made the code a little cleaner and easier to navigate

---
+++ Amun/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  Amun
+//
+//  Created by Richard Nkanga on 09/02/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationBarBackButtonHidden(true)
+    }
+
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Amun/OnboardingView.swift
@@ -0,0 +1,216 @@
+//
+//  OnboardingView.swift
+//  Amun
+//
+//  Created by Richard Nkanga on 09/02/2024.
+//
+
+import SwiftUI
+import CoreMotion
+
+
+
+struct OnboardingPageModel: Identifiable {
+    let id = UUID()
+    let title: String
+    let image: String
+}
+
+struct OnboardingView: View {
+
+    let page: [OnboardingPageModel] = [
+        OnboardingPageModel(title: "Know Everything about the Weather", image: "sun"),
+        OnboardingPageModel(title: "Weather Forecast with Some Humour", image: "wind"),
+        OnboardingPageModel(title: "Weather Forecast with Some Humour", image: "lightning"),
+    ]
+
+
+    @State private var currentIndex = 0
+    @State private var navigateToTestView = false
+
+    @State private var rotationAngle: Double = 0.0
+        let motionManager = CMMotionManager()
+
+
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.background)
+
+                VStack {
+                    //                    Text("Current Index = \(currentIndex)").font(.custom("Avenir-Black", size: 17))
+
+                    Spacer()
+
+                        TabView(selection: $currentIndex) {
+                            ForEach(page.indices, id: \.self) { p in
+
+                                VStack {
+                                    VStack {
+                                        Image(page[p].image)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame( height: 330)
+                                            .rotationEffect(.degrees(rotationAngle))
+                                            .onAppear {
+                                                startGyroscopeUpdates()
+                                            }
+                                        //                                                      .onDisappear {
+                                        //                                                          stopGyroscopeUpdates()
+                                        //                                                      }
+                                    }
+
+
+                                    VStack(spacing: 5){
+                                        Text(page[p].title).font(.custom("Avenir-Black", size: 37))
+                                            .lineLimit(nil)
+    //                                        .minimumScaleFactor(0.3)
+                                            .fixedSize(horizontal: false, vertical: true)
+
+                                        VStack {
+                                            Text("Start now and learn more about the")
+                                                .font(.custom("Avenir-Medium", size: 17))
+                                                .multilineTextAlignment(.center)
+                                                .lineLimit(2)
+
+                                            HStack(spacing: 5) {
+                                                Text("local weather")
+                                                    .font(.custom("Avenir-Black", size: 17))
+
+                                                Text("instantly")
+                                                    .font(.custom("Avenir-Medium", size: 17))
+                                            }
+                                        }
+                                        .opacity(0.5)
+                                    }
+
+
+                                }
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.text)
+
+
+
+                            }
+                        }
+                        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                        .frame(height: 500)
+
+
+
+                    Spacer()
+                        .frame(height: 41)
+
+
+
+                    Button(action: {
+                        if currentIndex < page.count - 1{
+                            currentIndex += 1
+                        }else{
+
+                            navigateToTestView = true
+                        }
+
+                    }, label: {
+                        Text(currentIndex < page.count - 1 ? "Next" : "Get Started")
+                            .font(.custom("Avenir-Black", size: 17))
+                            .frame(width: 260, height: 60, alignment: .center)
+
+                            .background {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .frame(width: 260, height: 60)
+                                        .foregroundStyle(.greyShadow)
+                                        .offset(x: -6, y: 6)
+
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .frame(width: 260, height: 60)
+                                        .foregroundStyle(.text)
+                                }
+                            }
+                            .foregroundColor(.textColorInversed)
+
+                    })
+                    .navigationDestination(isPresented: $navigateToTestView) {
+                                         HomeView()
+                                             .navigationBarBackButtonHidden(true)
+                                     }
+
+                    Spacer()
+                        .frame(height: 120)
+                }
+
+
+            }
+            .ignoresSafeArea(.all)
+            .toolbar {
+                ToolbarItem {
+
+                        Button(action: {
+
+
+                            // code
+
+                        }, label: {
+                            Text("Skip")
+                                .font(.custom("Avenir-Medum", size: 10))
+                                .frame(width: 58, height: 27, alignment: .center)
+
+                                .background {
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .frame(width: 58, height: 27)
+                                            .foregroundStyle(.greyShadow)
+                                            .offset(x: -4, y: 4)
+
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .frame(width: 58, height: 27)
+                                            .foregroundStyle(.text)
+                                    }
+                                }
+                                .foregroundColor(.textColorInversed)
+
+                    })
+
+                }
+            }
+        }
+    }
+
+
+
+
+    
+    func startGyroscopeUpdates() {
+           guard motionManager.isGyroAvailable else {
+               print("Gyroscope is not available.")
+               return
+           }
+
+           motionManager.gyroUpdateInterval = 0.1
+           motionManager.startGyroUpdates(to: .main) { data, _ in
+               guard let data = data else { return }
+
+               // Use rotation data to rotate the image
+               let rotationRate = data.rotationRate
+               let rotationAngle = atan2(rotationRate.y, rotationRate.x) * 90 / .pi
+
+               withAnimation {
+                   self.rotationAngle = rotationAngle
+               }
+           }
+       }
+
+       func stopGyroscopeUpdates() {
+           if motionManager.isGyroActive {
+               motionManager.stopGyroUpdates()
+           }
+       }
+
+
+
+}
+
+#Preview {
+    OnboardingView()
+}
