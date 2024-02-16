10a33c88039356ca4f13efdfe4feecd4f6132e79
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2024-02-15 19:49:42 -0500

    added and formatted the showing of present date for the current day

--- Amun/HomeView.swift
+++ Amun/HomeView.swift
@@ -8,24 +8,186 @@
 import SwiftUI
 
 struct HomeView: View {
-    
-    @State var locations = ["America", "Montreal", "Nigeria", "Poland", "London"]
+    @StateObject private var viewModel = WeatherViewModel()
+    @State var sampleData = ["Montreal", "Paris", "poland", "London"]
+    @State var locationArray: [String] = []
+    @State private var newLocation = "Canada"
+    @State private var isEditing = false
 
     var body: some View {
-        ZStack {
-              Color(.background)
-                .ignoresSafeArea(.all)
+        NavigationStack {
+            ZStack {
+                Color(.background)
+                    .ignoresSafeArea(.all)
+                    .onAppear {
+                    }
 
+                ScrollView {
+                    VStack {
+                        ForEach(viewModel.locations) { locs in
 
+                            HStack {
+                                HStack {
+                                    if (locs.weatherDataModel?.weather[0].description.lowercased().contains("cloud")) ?? false {
+                                        Image("wind")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 44, height: 44, alignment: .center)
+                                            .padding(.leading, 10)
 
+                                    } else {
+                                        Image("sun")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 44, height: 44, alignment: .center)
+                                            .padding(.leading, 10)
+                                    }
 
-            VStack{
+                                    Text(locs.name)
+                                        .font(.custom("Avenir-Black", size: 35))
+                                        .foregroundStyle(.text)
 
+                                    Spacer()
+
+                                    if let weatherData = locs.weatherDataModel {
+                                        Text("\(Int(weatherData.main.temp))°C")
+                                            .font(.custom("Avenir-Black", size: 25))
+                                            .foregroundStyle(.text)
+                                            .opacity(0.3)
+                                            .padding()
+                                        //                                Text("Temp: \(weatherData.name)")
+                                    } else {
+                                        Text("Loading....")
+                                    }
+                                }
+                                .frame(width: UIScreen.main.bounds.width, height: 85)
+//                                .background(Color(.weatherHolder))
+
+                                .background {
+                                    ZStack {
+                                        Rectangle()
+                                            .frame(width: UIScreen.main.bounds.width, height: 85)
+                                            .foregroundStyle(Color(.greyShadow))
+                                            .offset(y: 1)
+                                            .opacity(0.5)
+
+                                        Rectangle()
+                                            .frame(width: UIScreen.main.bounds.width, height: 85)
+                                            .foregroundStyle(Color(.weatherHolder))
+                                    }
+                                }
+                                .foregroundColor(.textColorInversed)
+//                                .cornerRadius(9)
+                                .padding(.bottom, 10)
+
+//                                .padding(.bottom, 14)
+                            }
+                        }
+                    }
+                    .onAppear {
+                        if locationArray.isEmpty {
+                            print("city is  empty")
+
+                            for data in sampleData {
+                                locationArray.append(data)
+                                let sampleLocationDate = Location(name: data)
+                                viewModel.locations.append(sampleLocationDate)
+                                viewModel.fetchWeather(for: sampleLocationDate)
+                            }
+                        } else {
+//                            print ("city is not empty")
+                        }
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                }
+
+                Spacer()
+
+                Button(action: {
+                    locationArray.append("Waterloo")
+                    let sampleLocationDate = Location(name: "Waterloo")
+                    viewModel.locations.append(sampleLocationDate)
+                    viewModel.fetchWeather(for: sampleLocationDate)
+                    print("Button Pressed")
+                }) {
+                    Image(systemName: "plus")
+                        .font(Font.custom("Avenir", size: 17.59).weight(.heavy))
+                        .frame(width: 70, height: 70, alignment: .center)
+//                                       .background(Color.text)
+                        .background {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 60, height: 60)
+                                    .foregroundStyle(.greyShadow)
+                                    .offset(x: -4, y: 3)
+
+                                Circle()
+                                    .frame(width: 60, height: 60)
+                                    .foregroundStyle(.text)
+                            }
+                        }
+                        .foregroundColor(.textColorInversed)
+                        .cornerRadius(9)
+                        .padding(.bottom, 30)
+                }
+                .offset(x: UIScreen.main.bounds.height * 0.17, y: UIScreen.main.bounds.height * 0.37)
             }
+            .background(Color.red)
+            .navigationTitle("Forecast")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading){
+                    Text(Date().formatted(.dateTime.day().weekday().month().year()))
+                }
+                ToolbarItem {
+                    Button(action: {
+                    }, label: {
+                        Text(isEditing ? "Done" : "Edit")
+                            .font(.custom("Avenir-Medum", size: 10))
+                            .frame(width: 58, height: 27, alignment: .center)
 
+                            .background {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .frame(width: 58, height: 27)
+                                        .foregroundStyle(.greyShadow)
+                                        .offset(x: -4, y: 4)
 
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .frame(width: 58, height: 27)
+                                        .foregroundStyle(.text)
+                                }
+                            }
+                            .foregroundColor(.textColorInversed)
+
+                    })
+                }
+
+                ToolbarItem {
+                    Button(action: {
+                    }) {
+                        Image(systemName: "gear")
+                            .font(Font.custom("Avenir", size: 14.59).weight(.heavy))
+                            .frame(width: 35, height: 35, alignment: .center)
+                            //                                       .background(Color.text)
+                            .background {
+                                ZStack {
+                                    Circle()
+                                        .frame(width: 30, height: 30)
+                                        .foregroundStyle(.greyShadow)
+                                        .offset(x: -4, y: 3)
+
+                                    Circle()
+                                        .frame(width: 30, height: 30)
+                                        .foregroundStyle(.text)
+                                }
+                            }
+                            .foregroundColor(.textColorInversed)
+                            .cornerRadius(9)
+                    }
+                }
+            }
         }
-        .background(Color.red)
     }
 }
 
--- Amun/WeatherDataModel.swift
+++ Amun/WeatherDataModel.swift
@@ -6,3 +6,135 @@
 //
 
 import Foundation
+
+//
+//  WeatherDataModel.swift
+//  Nkanga_Richard_8893021_lab8
+//
+//  Created by Richard Nkanga on 22/07/2023.
+//
+
+import Foundation
+
+
+// MARK: - Welcome
+struct WeatherDataModel: Codable {
+    let coord: Coord
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let type, id: Int
+    let country: String
+    let sunrise, sunset: Int
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main, description, icon: String
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+}
+
+class Location: ObservableObject, Identifiable {
+    let id = UUID()
+    @Published var name: String
+    @Published  var weatherDataModel: WeatherDataModel?
+
+    init(name: String) {
+           self.name = name
+       }
+
+}
+
+class WeatherViewModel: ObservableObject {
+    @Published var locations: [Location] = []
+
+    func fetchWeather(for location: Location) {
+        // Implement API request to OpenWeatherMap using URLSession
+        // You'll need to replace "YOUR_API_KEY" with your actual OpenWeatherMap API key
+        let apiKey = "8281a792c5f995747b19a57c8e52ea8d"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(location.name)&appid=\(apiKey)"
+//        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=mexico&appid=\(apiKey)"
+
+        guard let url = URL(string: urlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+
+                return }
+
+            do {
+                let decodedData = try JSONDecoder().decode(WeatherDataModel.self, from: data)
+                DispatchQueue.main.async {
+                    location.weatherDataModel = decodedData
+                    self.refreshUI()
+                    print("Weather data fetched successfully: \(decodedData)")
+                }
+            } catch {
+                print("Error decoding weather data: \(error)")
+            }
+        }.resume()
+    }
+    func refreshUI() {
+         DispatchQueue.main.async {
+             self.objectWillChange.send()
+         }
+     }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
