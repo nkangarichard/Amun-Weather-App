f40c0c39c8f6d380614a7aaf1cecb9b179473488
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2024-05-31 13:20:58 -0400

    added finishing touches ad finilized the app

---
+++ Amun/ErrorPopupView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorPopupView.swift
+//  Amun
+//
+//  Created by Richard Nkanga on 22/05/2024.
+//
+
+import SwiftUI
+
+struct ErrorPopupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorPopupView()
+}
---
+++ Amun/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Amun
+//
+//  Created by Richard Nkanga on 05/04/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
